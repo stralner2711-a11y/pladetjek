27389a149fa3f5a2c130f1604baa7487f66344a9
stralner2711-a11y <stralner2711@gmail.com>
2026-07-30 22:48:00 +0200

    Ret Android systemkanter og byg release 1.7.1

--- android/app/src/main/java/dk/pladetjek/app/MainActivity.java
+++ android/app/src/main/java/dk/pladetjek/app/MainActivity.java
@@ -1,6 +1,14 @@
 package dk.pladetjek.app;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -10,5 +18,24 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(AppUpdaterPlugin.class);
         registerPlugin(PlateTextRecognizerPlugin.class);
         super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().getDecorView().setBackgroundColor(Color.rgb(7, 21, 35));
+
+        WindowInsetsControllerCompat insetsController =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        insetsController.setAppearanceLightStatusBars(false);
+        insetsController.setAppearanceLightNavigationBars(false);
+
+        View appContent = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(appContent, (view, windowInsets) -> {
+            Insets safeArea = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.systemBars()
+                            | WindowInsetsCompat.Type.displayCutout()
+            );
+            view.setPadding(safeArea.left, safeArea.top, safeArea.right, safeArea.bottom);
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(appContent);
     }
 }
